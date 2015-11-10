d35c8eadeffefa9a6fc1580280286a2b71f96dec
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-11-10 09:59:38 -0600

    vc140 nuget packaging

--- build_autopkgs.cmd
+++ build_autopkgs.cmd
@@ -1,6 +1,12 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Release;Platform=x64
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Debug;Platform=x64
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Release;Platform=win32
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe vsprojects\protobuf.sln /t:libprotobuf /p:Configuration=Debug;Platform=win32
-powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf.autopkg
-powershell -ExecutionPolicy ByPass Write-NuGetPackage .\protobuf-vc100.autopkg
+Setlocal EnableDelayedExpansion
+
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\bin
+
+MSBuild.exe /m vsprojects\protobuf.sln /t:libprotobuf /p:VisualStudioVersion=14.0;PlatformToolset=v140;Configuration=Release;Platform=x64 || exit /b !ERRORLEVEL!
+MSBuild.exe /m vsprojects\protobuf.sln /t:libprotobuf /p:VisualStudioVersion=14.0;PlatformToolset=v140;Configuration=Debug;Platform=x64 || exit /b !ERRORLEVEL!
+MSBuild.exe /m vsprojects\protobuf.sln /t:libprotobuf /p:VisualStudioVersion=14.0;PlatformToolset=v140;Configuration=Release;Platform=win32 || exit /b !ERRORLEVEL!
+MSBuild.exe /m vsprojects\protobuf.sln /t:libprotobuf /p:VisualStudioVersion=14.0;PlatformToolset=v140;Configuration=Debug;Platform=win32 || exit /b !ERRORLEVEL!
+
+set PSModulePath=%PSModulePath%;C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp
+powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\protobuf.autopkg || exit /b !ERRORLEVEL!
+powershell -ExecutionPolicy ByPass ipmo \"C:\Program Files (x86)\Outercurve Foundation\Modules\CoApp\CoApp.Powershell.Tools.dll\" ; Write-NuGetPackage .\protobuf-vc140.autopkg || exit /b !ERRORLEVEL!
